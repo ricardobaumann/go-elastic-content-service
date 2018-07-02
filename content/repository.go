f6766a219a59e044d77eb057a07dc7b15a317ea3
Ricardo Baumann <ricardo.luis.baumann@gmail.com>
2018-07-02 18:15:46 +0200

    wip

--- content/repository.go
+++ content/repository.go
@@ -1,23 +1,38 @@
 package content
 
 import (
-	"github.com/jmoiron/sqlx"
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/olivere/elastic"
 )
 
 //Data ...
 type repository struct {
-	DB *sqlx.DB
+	esCli *elastic.Client
 }
 
 // Create Repository
-func NewRepository(db *sqlx.DB) Repository {
-	return repository{DB: db}
+func NewRepository() Repository {
+	client, err := elastic.NewClient(elastic.SetURL("http://localhost:9200"), elastic.SetSniff(false))
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+	return repository{esCli: client}
 }
 
 func (r repository) Get(ID string) (contentResponse, error) {
 
 	//you can add your logic for database
 	//r.DB.Get(&your_db_object,"your_sql_query",your_params1, ...)
+	result, err := r.esCli.Get().Index("contents").Id(ID).Do(context.Background())
+	if err != nil {
+		fmt.Print("error")
+	}
+
+	fmt.Print(result.Source)
 
 	return contentResponse{ID}, nil
 }
