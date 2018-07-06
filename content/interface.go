93488574a084aade84417680712ff6b915bb7b47
Ricardo Baumann <ricardo.luis.baumann@gmail.com>
2018-07-06 15:40:04 +0200

    delete support

--- content/interface.go
+++ content/interface.go
@@ -6,10 +6,12 @@ import (
 
 type Service interface {
 	Get(ctx context.Context, obj contentRequest) (contentResponse, error)
+	Delete(ctx context.Context, obj contentRequest) (contentResponse, error)
 	Save(ctx context.Context, obj contentInput) (contentResponse, error)
 }
 
 type Repository interface {
 	Get(ID string) (contentResponse, error)
+	Delete(ID string) (contentResponse, error)
 	Save(ID string, Body string) (contentResponse, error)
 }
